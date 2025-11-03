3592ee35ad8ea49d1f39e81603c75b59dea3b19b
Michal Czycza <s28517@pjwstk.edu.pl>
2025-11-03 15:26:58 +0100

    a

--- user-service/src/config/db.js
+++ user-service/src/config/db.js
@@ -1,16 +1,32 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        port: process.env.DB_PORT,
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
         logging: false,
-    }
-);
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            port: process.env.DB_PORT,
+            logging: false,
+        }
+    );
+}
 
 module.exports = sequelize;
