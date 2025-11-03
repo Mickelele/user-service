4ec579f44dab66fb0674013fa5815c02aa03258f
Michal Czycza <s28517@pjwstk.edu.pl>
2025-11-03 15:18:18 +0100

    host2

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -7,8 +7,9 @@ const userRouter = require('./modules/users/user.routes');
 
 const app = express();
 
+
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
@@ -20,15 +21,16 @@ app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/user', userRouter);
 
+const PORT = process.env.PORT || 3000;
+
 (async () => {
     try {
         await sequelize.authenticate();
-        console.log('Połączono z PostgreSQL przez Sequelize');
+        console.log('Polaczono z PostgreSQL przez Sequelize');
         await sequelize.sync();
         console.log('Modele zsynchronizowane');
 
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => console.log(` Serwer działa na http://localhost:${PORT}`));
+        app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
     } catch (err) {
         console.error('Błąd połączenia z bazą:', err);
     }
