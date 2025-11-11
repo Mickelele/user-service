0ddc0bd8c54787e04f78ee8ef71e01a4b3df0229
Michal Czycza <s28517@pjwstk.edu.pl>
2025-11-12 00:59:09 +0100

    test student 2

---
+++ course-service/src/modules/group/student.model.js
@@ -0,0 +1,37 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../../config/db');
+
+class Uczen extends Model {}
+
+Uczen.init({
+    id_ucznia: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false
+    },
+    id_grupa: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    Opiekun_id_opiekuna: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'opiekun_id_opiekuna'
+    },
+    saldo_punktow: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    pseudonim: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'Uczen',
+    tableName: 'uczen',
+    timestamps: false
+});
+
+module.exports = Uczen;
