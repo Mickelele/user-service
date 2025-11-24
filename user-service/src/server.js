bdc49e83d1f21ee847907dfc32816dc23e32933c
Michal Czycza <s28517@pjwstk.edu.pl>
2025-11-24 17:04:56 +0100

    drobne poprawki

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 const { sequelize } = require('./config/db');
 const userRouter = require('./modules/users/user.routes');
 const teacherRouter = require('./modules/teacher/teacher.routes');
