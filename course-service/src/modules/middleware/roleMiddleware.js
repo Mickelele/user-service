f532f412a9568f145aa9b4a7abda9e884218dd80
Michal Czycza <s28517@pjwstk.edu.pl>
2026-01-16 11:50:28 +0100

    poprawa sprawdzenia uczniow dla opiekuna3

--- course-service/src/modules/middleware/roleMiddleware.js
+++ course-service/src/modules/middleware/roleMiddleware.js
@@ -52,7 +52,7 @@ const checkGuardianStudent = (studentIdParamName = 'userId') => {
 
            
             const axios = require('axios');
-            const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://localhost:4000';
+            const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'https://user-service-hg4z.onrender.com';
             const response = await axios.get(`${USER_SERVICE_URL}/opiekunowie/${guardianId}/uczniowie`, {
                 headers: { Authorization: req.headers.authorization }
             });
--- quiz-service/src/middleware/roleMiddleware.js
+++ quiz-service/src/middleware/roleMiddleware.js
@@ -51,7 +51,7 @@ const checkGuardianStudent = (studentIdParamName = 'studentId') => {
             const guardianId = req.user.id;
 
             const axios = require('axios');
-            const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://localhost:4000';
+            const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'https://user-service-hg4z.onrender.com';
             const response = await axios.get(`${USER_SERVICE_URL}/opiekunowie/${guardianId}/uczniowie`, {
                 headers: { Authorization: req.headers.authorization }
             });
