84f8131a9e9a6833025dab5ba00a037b20949419
Michal Czycza <s28517@pjwstk.edu.pl>
2026-01-16 12:11:20 +0100

    koncowki nauczuciela zabezpieczenie3

--- course-service/src/modules/comment/comment.routes.js
+++ course-service/src/modules/comment/comment.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UwagaController = require('./comment.controller');
-const auth = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 const { checkRole, checkGuardianStudent, checkTeacherStudent } = require('../middleware/roleMiddleware');
 
 router.get('/', UwagaController.getAll);
-router.get('/student/:id_ucznia', auth, checkRole(['uczen', 'opiekun', 'nauczyciel']), checkGuardianStudent('id_ucznia'), checkTeacherStudent('id_ucznia'), UwagaController.getByStudentId);
+router.get('/student/:id_ucznia', authMiddleware, checkRole(['uczen', 'opiekun', 'nauczyciel']), checkGuardianStudent('id_ucznia'), checkTeacherStudent('id_ucznia'), UwagaController.getByStudentId);
 router.get('/:id', UwagaController.getOne);
 
 router.post('/', authMiddleware, checkRole(['nauczyciel']), UwagaController.create);
